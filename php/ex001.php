6c120a62bed191fcd21fa52fdd0605f99f3e2780
Flávio Marques <121177356+marquesflavio@users.noreply.github.com>
2023-03-31 19:01:36 -0300

    estudo de laços com php

---
+++ php/ex001.php
@@ -0,0 +1,9 @@
+<?php
+    for($num1=1; $num1<=9; $num1++){
+        for($num2=1; $num2 <=10; $num2++){
+            echo "$num1 * $num2 = " . $num1 * $num2;
+            echo "<br>";
+        }
+        echo "<br>"
+    }
+?>
